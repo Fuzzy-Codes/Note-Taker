38d52705b54a597b7667664a232955fd30433b7d
alexc <alexcolwell13@gmail.com>
2023-09-05 12:28:54 -0500

    setup post route for creating a note and writing it to the db file

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,13 @@ router.get("/notes", (req, res) => {
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+router.post("/notes", (req, res) => {
+    const createNote = req.body;
+    createNote.id = uuidv4();
+
+    readAndAppend(createNote, "./db/db.json");
+    res.json(createNote);
+});
 
 
 module.exports = router;
